20fadb17b4d89fca2d8b1464eea64a2580293691
kushrami <kushrami16@gmail.com>
2020-12-12 15:00:18 +0530

    C program for showing structure of c program.

---
+++ C_01_01.c
@@ -0,0 +1,40 @@
+/*
+
+In this we are discussing stucture of c program.
+
+------------
+comments
+Preprocessor Directives
+Global variables
+main() function
+{
+    local variable...
+    statements
+    methods calls
+    assembly code if aby
+    .....
+}
+
+function1()
+{
+    local variable...
+    statements
+    methods calls
+    assembly code if aby
+    .........
+}
+
+function2()
+{
+    local variable...
+    statements
+    methods calls
+    assembly code if aby
+    .........
+}
+
+Most of the c programms are written in this way.
+After code size increses every thing goes to libs and modules.
+The Object Oriented concept are not here but idea is here.
+
+*/
