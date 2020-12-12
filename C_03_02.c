476346c950cb9db37ee2a46b771b740cfcc83e2d
kushrami <kushrami16@gmail.com>
2020-12-12 15:00:49 +0530

    C program for character io.macros getchar() and putchar().

---
+++ C_03_02.c
@@ -0,0 +1,11 @@
+//This program is of character io, macros are there getchar() and putchar() can be 
+//specially for charaters io.
+#include <stdio.h>
+void main()
+{
+    char ch;
+    printf("Enter a  character:");
+    ch = getchar();
+    printf("You have entered: ");
+    putchar(ch);
+}
